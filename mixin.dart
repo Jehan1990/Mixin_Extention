acb703585acca853824dc889b4cd4d651c607ff2
jehan ichhaporia <jehan_ichhaporia@hotmail.com>
2025-11-05 15:31:50 +0530

    add

---
+++ mixin.dart
@@ -0,0 +1,22 @@
+
+mixin Swimming{
+  void swim()=>print('Swimming');
+}
+
+class Animal{
+  void sleeping()=>print('Sleeping');
+}
+
+class Fish extends Animal with Swimming{
+
+}
+
+class Human extends Animal with Swimming{
+
+}
+
+void main(){
+  final fish=Fish();
+  fish.sleeping();//Parent Class
+  fish.swim();//Mixin
+}
