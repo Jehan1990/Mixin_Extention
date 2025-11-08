3b2f694e8f764c326a3ab79f0b3aaafbf22bbf0e
jehan ichhaporia <jehan_ichhaporia@hotmail.com>
2025-11-08 16:04:43 +0530

    addd

--- general_extension.dart
+++ general_extension.dart
@@ -1,4 +1,8 @@
-extension on String {
+
+///DoubleParsing is the name of extension. If extension wants to be used then we need 
+///to name it.
+ 
+extension DoubleParsing on String {
   double? toDoubleOrNull() => double.tryParse(this);
   //this referes to the variable which is used as a string
 }
